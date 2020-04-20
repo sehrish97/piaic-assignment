e40618c674adfff7c9254930d4b04e9c758de579
sehrish97 <mishi3231@gmail.com>
2020-04-20 17:08:14 +0500

    first commit

---
+++ piaicassignment1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+Add=num1+num2
+print(Add)
+subtract=num1-num2
+print(subtract)
+multiply=num1*num2
+print(multiply)
+divide=num1/num2
+print(divide)
